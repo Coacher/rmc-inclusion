4eec43f69f9878d9ca434f831b030e4f7a920370
Ilya Tumaykin <itumaykin@gmail.com>
2013-04-15 15:56:34 +0400

    Make gen_graph.sh script aware of the fact that rmc-short generates TR graphs

--- tools/gen_graph.sh
+++ tools/gen_graph.sh
@@ -4,8 +4,7 @@ if [ -d "$1-$2-$3" ]; then
     cd "$1-$2-$3"
 
     if [ -f "inclusion_tree_short_$1-$2-$3.gv" ]; then
-        tred "inclusion_tree_short_$1-$2-$3.gv" > "inclusion_tree_short_$1-$2-$3_reduced.gv"
-        dot -Tsvg "inclusion_tree_short_$1-$2-$3_reduced.gv" > "inclusion_tree_short_$1-$2-$3.svg"
+        dot -Tsvg "inclusion_tree_short_$1-$2-$3.gv" > "inclusion_tree_short_$1-$2-$3.svg"
     fi
 
     if [ -f "inclusion_tree_$1-$2-$3.gv" ]; then
