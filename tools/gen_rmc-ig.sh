004cf79074633ded82cca65a6eb858d9dd36b6fd
Ilya Tumaykin <itumaykin@gmail.com>
2013-04-17 03:16:33 +0400

    Allow to specify o_weight in gen_rmc-ig.sh

--- tools/gen_rmc-ig.sh
+++ tools/gen_rmc-ig.sh
@@ -2,6 +2,7 @@
 
 M_WEIGHT="${5:-1000}"
 R_WEIGHT="${6:-1000}"
+O_WEIGHT="${7:-1000}"
 
 if [ -d "$1" ]; then
     if [ -x "$1/rmc-ig" ]; then
@@ -10,7 +11,7 @@ if [ -d "$1" ]; then
         fi
 
         cd "$2-$3-$4" && \
-        "../$1/rmc-ig" -p $2 -l $3 -L $4 -IGR -m ${M_WEIGHT} -r ${R_WEIGHT} || exit 1;
+        "../$1/rmc-ig" -p $2 -l $3 -L $4 -IGR -m ${M_WEIGHT} -r ${R_WEIGHT} -o ${O_WEIGHT} || exit 1;
     else
         echo "$1/rmc-ig is not found. You should build it."
         exit 1;
--- tools/gen_rmc_full.sh
+++ tools/gen_rmc_full.sh
@@ -6,5 +6,5 @@ O_WEIGHT="${6:-1000}"
 
 rm -rf "$1-$2-$3" && mkdir "$1-$2-$3" && \
 tools/gen_rmc-short.sh build/src $1 $2 $3 ${M_WEIGHT} ${R_WEIGHT} ${O_WEIGHT} && \
-tools/gen_rmc-ig.sh build/src $1 $2 $3 ${M_WEIGHT} ${R_WEIGHT} && \
+tools/gen_rmc-ig.sh build/src $1 $2 $3 ${M_WEIGHT} ${R_WEIGHT} ${O_WEIGHT} && \
 tools/gen_graph.sh $1 $2 $3
